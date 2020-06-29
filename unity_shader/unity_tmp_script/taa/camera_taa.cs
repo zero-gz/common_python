9ece56dc4b9ca1b63fcf98d70d9916bfa301ee8a
zero-gz <hust_sdxz@163.com>
2020-06-29 21:56:03 +0800

    add the new taa cs

--- unity_shader/unity_tmp_script/taa/camera_taa.cs
+++ unity_shader/unity_tmp_script/taa/camera_taa.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class camera_taa : MonoBehaviour {
     private Camera m_cam;
     private Material m_mat;
+    private RenderTexture pre_tex;
+
+    [Range(0.0f, 1.0f)] public float feedbackMin = 0.88f;
+    [Range(0.0f, 1.0f)] public float feedbackMax = 0.97f;
 
     // Use this for initialization
     void Start () {
@@ -17,6 +22,9 @@ public class camera_taa : MonoBehaviour {
         m_cam.depthTextureMode = DepthTextureMode.Depth | DepthTextureMode.MotionVectors;
 
         m_mat = new Material(Shader.Find("my_shader/custom_taa"));
+        pre_tex = new RenderTexture(Screen.width, Screen.height, 0);
+
+        Shader.SetGlobalTexture(Shader.PropertyToID("_PrevTex"), pre_tex);
     }
 
     // Update is called once per frame
@@ -26,6 +34,10 @@ public class camera_taa : MonoBehaviour {
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        m_mat.SetFloat("_FeedbackMin", feedbackMin);
+        m_mat.SetFloat("_FeedbackMax", feedbackMax);
+
         Graphics.Blit(source, destination, m_mat);
+        Graphics.Blit(destination, pre_tex);
     }
 }
